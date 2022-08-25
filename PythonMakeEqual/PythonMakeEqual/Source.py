5c9cb34625e37427a2edcb96f5e52f4807a43045
SH-NicholasPage <npage@southhills.edu>
2022-08-24 23:02:45 -0400

    Added comments

--- PythonMakeEqual/PythonMakeEqual/Source.py
+++ PythonMakeEqual/PythonMakeEqual/Source.py
@@ -1,3 +1,13 @@
-def makeEqual(arr1, arr2):
+#
+# Name: [YOUR NAME HERE]
+# South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+#
 
+#
+#	Takes two integer arrays as inputs
+#	Return how many operations it takes to make the two arrays equal
+#	Return -1 if the two arrays cannot be made equal or constraints are not met
+#
+def makeEqual(arr1, arr2):
+    #TODO: EDIT THIS
     return -1
